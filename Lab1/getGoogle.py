4128f32691439ab341cd20cf5b2d994bb62dd3ff
fujishig2 <fujishig@ualberta.ca>
2021-01-14 16:31:43 -0700

    Updated to get the headers, encoding, status code, and source code of the raw URL

--- Lab1/getGoogle.py
+++ Lab1/getGoogle.py
@@ -1,4 +1,7 @@
 import requests
 
-r = requests.get('https://www.google.com')
-print(r)
+r = requests.get('https://raw.githubusercontent.com/fujishig2/CMPUT404/main/Lab1/getGoogle.py')
+print('Status Code:', r, '\n')
+print('Headers:', r.headers, '\n')
+print('Encoding:', r.encoding, '\n')
+print('Text:', '\"' + r.text + '\"')
